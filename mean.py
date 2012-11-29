4e41f1b86d01f2b81505a92081f8ac0641c672f3
Renee Jutras <rmjutras@gmail.com>
2012-11-29 14:43:50 -0500

    Added comments to describe program

--- mean.py
+++ mean.py
@@ -1,14 +1,16 @@
 # Prints mean of the numbers given as arguments.
 import sys
 
-
+# Error checking for case of zero arguments
 if len(sys.argv)==1:
 	print 'Error: No arguments given.'
 	sys.exit()
 
 sum = 0
 
+#Takes sum of all inputted numbers
 for num in sys.argv[1:]:
 	sum+= float(num)
 
+#Calculates and prints the average
 print sum / (len(sys.argv)-1
